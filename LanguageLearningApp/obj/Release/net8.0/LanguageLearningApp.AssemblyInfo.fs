d3a3a32f170cbd0181f1e6ae70e44c2f9d098e21
glebas4567 <glebskov1121@gmail.com>
2025-02-16 19:32:57 +0300

    Window5(Gleb):Statistics.

--- LanguageLearningApp/obj/Release/net8.0/LanguageLearningApp.AssemblyInfo.fs
+++ LanguageLearningApp/obj/Release/net8.0/LanguageLearningApp.AssemblyInfo.fs
@@ -8,7 +8,7 @@ open System.Reflection
 
 
 [<assembly: System.Reflection.AssemblyCompanyAttribute("LanguageLearningApp")>]
-[<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
+[<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1004ac11c9f2a4db187c8588e62124a7108ad2d3")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("LanguageLearningApp")>]
